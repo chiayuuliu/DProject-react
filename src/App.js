d9bc69755dbd45b51aa8fc128523dc69eb8f4e02
Doris Liu <88422446+chiayuuliu@users.noreply.github.com>
2021-10-23 23:48:51 +0800

    修改scss路徑名稱, 篩選區取消form 改用div包

--- src/App.js
+++ src/App.js
@@ -20,14 +20,17 @@ function App() {
       <>
         <Switch>
         {/* 商品細節頁 */}
+        {/* 實際路由/product-detail/:id */}
           <Route path="/product-detail">
             <ProductDetail />
           </Route>
           {/* 商品頁 */}
+          {/* 其他頁面路由/cate=1,/cate=2,/cate=3,/all */}
           <Route path="/products">
             <Products />
           </Route>
           {/* 客製化推薦 */}
+          {/* 選不同的路由要變??? */}
           <Route path="/client-target">
             <Customized />
           </Route>
--- src/components/Product/Filter.js
+++ src/components/Product/Filter.js
@@ -1,20 +1,23 @@
 import React from 'react'
 
 function Filter(props) {
+  const { searchWord, setSearchWord } = props
   return (
     <>
-      <form action="" className="d-flex pd-search-wrap">
+      <div className="d-flex pd-search-wrap">
         <button type="button" className="pd-search-icon">
           <i className="fas fa-search "></i>
         </button>
+        {/* 關鍵字搜尋 */}
         <input
           type="text"
           className="pd-search-input"
-          name="search"
-          id="search"
           placeholder="請輸入關鍵字"
+          value={searchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
         />
-      </form>
+      </div>
+      {/* 篩選器 */}
       <div className="pd-radio-wrap">
         <input type="radio" name="option" id="low" />
         <label for="low">低熱量優先</label>
